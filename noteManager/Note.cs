1c864be9d7048dcdb25c5d07cea3defa9dc23e8d
weed <rivenmle@gmail.com>
2021-04-18 19:09:47 +0700

    Add project files.

---
+++ noteManager/Note.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.IO;
+
+namespace noteManager
+{
+    public class Note
+    {
+        private string Filename { get; set; }
+        public Note(string filename)
+        {
+            this.Filename = $"./{filename}.json";
+            if (!File.Exists(this.Filename))
+            {
+                Hashtable data = new Hashtable();
+                string jsonData = JsonSerializer.Serialize(data);
+                File.WriteAllText(this.Filename, jsonData);
+            }
+        }
+
+        public Hashtable GetAllNotes()
+        {
+            string jsonData = File.ReadAllText(this.Filename);
+            Hashtable notes = JsonSerializer.Deserialize<Hashtable>(jsonData);
+
+            return notes;
+        }
+    }
+}
---
+++ noteManager/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+namespace noteManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            (new Program()).showMainMenu();
+            Note noteManager = new Note("test");
+            foreach (DictionaryEntry note in noteManager.GetAllNotes())
+            {
+                Console.WriteLine(note.Value + " " + note.Key);
+            }
+            Console.ReadLine();
+        }
+
+        private void showMainMenu()
+        {
+            Console.WriteLine("1. List all notes \n" +
+                "2. Add new note \n" +
+                "3. Edit note(with id) \n" +
+                "4. Delete note(with id)");
+        }
+    }
+}
